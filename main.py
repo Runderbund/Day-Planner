b17e502f169049856b6af13b090f93c13218fc14
unknown <sean.herbison@gmail.com>
2023-01-28 11:28:48 -0700

    Pushes third commit with some code. Trying Switch Case for first time.

--- main.py
+++ main.py
@@ -6,7 +6,27 @@ def generate_trip():
     transportation = ["Train", "Uber", "Airplane", "Helicopter", "Segway", "Walk", "Horse"]
     entertainment = ["See a play", "Watch a movie", "Go bowling", "Take a walk", "Ride Horses", "Read a book", "Watch paint dry"]
     trip_aspect_list = [destinations, restaurants, transportation, entertainment]
-    pass
+
+def make_initial_plan(trip_aspect_list):
+    destination = trip_aspect_list[0][random.randint(0, 6)]
+    restaurant = trip_aspect_list[1][random.randint(0, 6)]
+    transportation = trip_aspect_list[2][random.randint(0, 6)]
+    entertainment = trip_aspect_list[3][random.randint(0, 6)]
+
+def change_trip_aspect(trip_aspect_list, item_to_change):
+    match item_to_change:
+        case 1:
+            destination = trip_aspect_list[0][random.randint(0, 6)]
+            return destination
+        case 2:
+            restaurant = trip_aspect_list[1][random.randint(0, 6)]
+            return restaurant
+        case 3:
+            transportation = trip_aspect_list[2][random.randint(0, 6)]
+            return transportation
+        case 4:
+            entertainment = trip_aspect_list[3][random.randint(0, 6)]
+            return entertainment
 
 def print_trip(options):
     pass
